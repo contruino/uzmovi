82339692915b6c7aeb59c5103efed46bb80e35f3
Pyprofessor <119044669+contruino@users.noreply.github.com>
2022-12-01 16:35:20 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,10 +1,34 @@
 from flask import Flask
 from flask_restful import Resource,Api
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import requests
+from bs4 import *
 
 app = Flask(__name__)
 api = Api(app)
-class Home(Resource):
-    def get(self, name):
-        return {'ism':name}
 
-api.add_resource(Home, "/<string:name>")
+class Home(Resource):
+    def get(self, search):
+        sayt=f"https://www.uzmovi.com/search?q={search}"
+
+        film=requests.get(sayt).text
+        soup=BeautifulSoup(film, "lxml")
+        kino=soup.find_all(class_="shortstory-in categ")
+
+        options=webdriver.ChromeOptions()
+        driver=webdriver.Chrome(executable_path="chromedriver.exe",options=options)
+
+        for kn in kino:
+            films=kn.find(class_="short-images-link").get("href")
+            print(films)
+            driver.get(films)
+        
+            info=driver.find_element(By.CLASS_NAME, "finfo").get_attribute("innerText")
+            print(info)
+            return "bajarildi"
+
+api.add_resource(Home, "/<string:search>")
+
+app.run(debug=True)
